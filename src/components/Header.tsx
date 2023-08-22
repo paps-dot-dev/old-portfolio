b60b1c00eede2ce4585cf96ffd5969ce0bab7cd5
Shawn <s.papineau92@gmail.com>
2023-08-22 14:31:18 -0400

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,15 +2,18 @@ import React from 'react'
 
 function Header() {
   return (
-    <header className='flex flex-col justify-between m-4'>
-      <p>Moon</p>
-      <h1 className='text-center text-5xl p-2 md:text-[64px]'>paps.dev</h1>
-      <ul className='flex m-auto md:ml-auto md:mr-0'>
-        <li>Social 1</li>
-        <li>Social 2</li>
-        <li>Social 3</li>
-        <li>Social 4</li>
-      </ul>
+    // Change
+    <header className='flex flex-col justify-between  md:m-4 md:py-[90px] py-10'>
+      {/* Megans suggestion: Add Image Carousel (use locomotion and do cards!)the size of header div, move paps.dev to the left side */}
+      <div className='flex justify-between items-center w-full'>
+        <h1 className='text-center text-3xl p-2 md:text-[48px]'>paps.dev</h1>
+        <ul className='flex m-auto md:ml-auto md:mr-0'>
+          <li>Social 1</li>
+          <li>Social 2</li>
+          <li>Social 3</li>
+          <li>Social 4</li>
+        </ul>
+      </div>
       <div className='w-[95vw] border-b-2 border-black m-auto p-1'></div>
     </header>
   )
